63d82d522076a4b1b64e76efe8d61a1f3e2b5815
whiplade <nmawira13@gmail.com>
2023-09-19 17:31:17 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python3
 
 from flask import Flask
-import operator 
 
 app = Flask(__name__)
-
-if __name__ == '__main__':
-    app.run(port=5555, debug=True)
 
 @app.route('/')
 def index():
@@ -22,6 +18,22 @@ def count(parameter):
     numbers = "\n".join(str(i) for i in range(parameter + 1))
     return numbers
 
-@app.route('/math/<float:num1><operation><float:num2>')
+@app.route('/math/<int:num1>/<operation>/<int:num2>')
 def math(num1, operation, num2):
-    return 
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':
+        result = num1 / num2
+    elif operation == '%':
+        result = num1 % num2
+    else:
+        result = "Please input a valid operation!"
+
+    return str(result)
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
